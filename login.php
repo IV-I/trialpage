3aa7714a76a7dab1fb0ff559b78dcf09e03fe43c
IV-I <aifai_5@yahoo.com>
2019-02-15 15:09:53 +0000

    Page

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+<form id="login" method="post”
+    action="validate.php">
+Username: <input type="text" name="user">
+<br/>
+Password: <input type="password"
+                 name="password"> <br/>
+<button type="submit">Login</button>
+</form>
+</html>
+
+/**
+ * Created by PhpStorm.
+ * User: 1212903
+ * Date: 15/02/2019
+ * Time: 15:04
+ */
